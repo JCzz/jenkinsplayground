3f99bcfee6dd62dbe57c6a0d7a06229402ad5419
JCzz <awear@appsyouwear.com>
2022-11-27 20:13:58 +0100

    demo

--- secrets/docker_secret.sh
+++ secrets/docker_secret.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=JCzz --docker-password=dckr_pat_GmBay1smA0nM4VH61momTPnPfGg --docker-email=awear@appsyouwear.com -n devops-tools 
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=JCzz --docker-password=wynVos-bijjev-0qutko --docker-email=awear@appsyouwear.com -n devops-tools 
+
+kubectl delete docker-registry regcred -n devops-tools 
 
 # kubectl create secret generic regcred \
 #    --from-file=.dockerconfigjson=<path/to/.docker/config.json> \
--- secrets/sopy_from_m.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-multipass transfer master:.kube/config .
